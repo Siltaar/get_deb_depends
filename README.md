e6899c0134a099e851319ef607ac3e5092a81bdd
Siltaar <siltaar@d12s.fr>
2017-06-10 14:26:05 +0200

    becomes recursive

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 ## Usage
 
-`./get_deb_depends.sh <package-name> <depends-string> [<wanted-architecture>]`
+`./get_deb_depends.sh <package-name> [<wanted-architecture>]`
 
-* `depends-string` is the text part of your (may be) localized `apt-cache depends` answer ("Dépend" in french)
-* `wanted-architecture`, if using multiarch you can specify which arch to download: amd64, i386… (defaults to 'amd64')
+`wanted-architecture` : if using multiarch you can specify which arch to download: amd64, i386… (defaults to 'amd64')
 
-Strongly inspired from : https://stackoverflow.com/a/39968534
+Strongly inspired from better answers of this thread : https://stackoverflow.com/a/39968534
 
-Date : 2017-06-10
+Depends on `apt-rdepends`.
 
-Licence : GPLv3 (see the LICENCE file)
-
-Author : Simon Descarpentries <siltaar /\ acoeuro.com>
+* Date : 2017-06-10
+* Licence : GPLv3 (see the LICENCE file)
+* Author : Simon Descarpentries <siltaar /\ acoeuro.com>
--- get_deb_depends.sh
+++ get_deb_depends.sh
@@ -4,30 +4,17 @@
 # Licence : GPLv3 (see the LICENCE file)
 # Author : Simon Descarpentries <siltaar /\ acoeuro.com>
 
-# Usage : ./get_deb_depends.sh <package-name> <depends-string> [<wanted-architecture>]
-#	depends-string is the text part of your (may be) localized `apt-cache depends` answer :
-#		("Dépend" in french)
+# Usage : ./get_deb_depends.sh <package-name> [<wanted-architecture>]
 #	wanted-architecture, if using multiarch you can specify which arch to download: amd64, i386…
 #		(defaults to 'amd64')
 
-# Strongly inspired from : https://stackoverflow.com/a/39968534
-
+# Strongly inspired from solutions of this thread : https://stackoverflow.com/a/39968534
 
 pkg_name=$1
-depends_str=$2
-arch=$3 || 'amd64'
-tmp_file=/tmp/get_deb_depends
+arch=$2 || 'amd64'
 
-apt-cache depends "$pkg_name" | grep $depends_str: >> "$tmp_file"
-
-sed -i -e 's/[<>|:]//g' "$tmp_file"
-sed -i -e "s/$depends_str//g" "$tmp_file"
-sed -i -e 's/ //g' "$tmp_file"
-
-while read -r line
-do
-    apt-get download "$line":"$arch"
-done < "$tmp_file"
+for dep in `apt-rdepends "$pkg_name" | grep -v "^ "`; do
+    apt-get download "$dep":"$arch"
+done
 
 apt-get download "$pkg_name":"$arch"
-rm $tmp_file
